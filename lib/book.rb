4194574926954ad73f70889bcac7f3ef07c3a4da
Leslie Huang <lesliehuang@gmail.com>
2017-06-29 18:56:54 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
 
 
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  attr_accessor :genre, :page_count, :author
+
+
+  def turn_page
+      puts "Flipping the page...wow, you read fast!"
+  end
+
+end
