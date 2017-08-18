f7d57cee5bd2e32f4e4334fb4119bad15bb663e7
Mikheil Tkavadze <tkavadze@gmail.com>
2017-08-18 13:58:58 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
 
 
+class Book
+attr_accessor :name, :title, :page_count, :genre, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
