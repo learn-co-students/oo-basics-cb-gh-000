1253955a498c94fcd9aba3971f014f88b02ad096
James Kim <jamesgki.kim67@gmail.com>
2017-08-29 23:49:34 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+class Book
 
+  def initialize(title)
+    @title = title
+  end
 
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
