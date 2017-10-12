b2f575a569ba6a307fb9851a5ee1c7ff42683537
Arpith Muddi <arpithgre@gmail.com>
2017-10-12 07:19:05 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+  def turn_page=(count)
+    @turn_page = count
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+  attr_accessor :title, :author, :page_count, :genre
+end
