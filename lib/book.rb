5ddcc9991466f7835aea8242a06fb6b5b2565e62
Edgar V <evenegas9@ucmerced.edu>
2019-09-11 07:34:28 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
+class Book
+  attr_accessor :author, :page_count, :title, :genre
+  def initialize title
+    @title = title #instance variable
+  end
 
-
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
